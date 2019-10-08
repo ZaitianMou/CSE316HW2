b189cb769d99fa4e89f3f8cfb512b64f9c9eb31a
Zaitian <Thomas@ThomasMoudeMacBook-Pro.local>
2019-10-08 13:49:32 -0400

    sort done

--- src/components/list_screen/ListItemsTable.js
+++ src/components/list_screen/ListItemsTable.js
@@ -22,26 +22,88 @@ export class ListItemsTable extends Component {
         )
     }
     sortByItemDescription = () => {
+        if (this.props.todoList.currentSortCriteria=="sortByItemDescriptionIncrease")
+            {this.props.todoList.currentSortCriteria="sortByItemDescriptionDecrease";}
+        else 
+            {this.props.todoList.currentSortCriteria="sortByItemDescriptionIncrease";}
+
         this.props.todoList.items.sort(this.compareByDescription);
-        console.log(this.props)
         this.props.goHome();
     }
-    compareByDescription(item1, item2) {
-        if (item1.description < item2.description)
-            return -1;
-        else if (item1.description > item2.description)
+    compareByDescription =(item1,item2) => {
+        if (this.props.todoList.currentSortCriteria=="sortByItemDescriptionIncrease"){
+            if (item1.description < item2.description)
+                return -1;
+            else if (item1.description > item2.description)
+                return 1;
+            else
+                return 0;
+        }
+        else {
+            if (item1.description < item2.description)
             return 1;
+        else if (item1.description > item2.description)
+            return -1;
         else
             return 0;
+        }
+    }
+
+    sortByItemDueDate = () => {
+        if (this.props.todoList.currentSortCriteria=="sortByItemDueDateIncrease")
+            {this.props.todoList.currentSortCriteria="sortByItemDueDateDecrease";}
+        else 
+            {this.props.todoList.currentSortCriteria="sortByItemDueDateIncrease";}
         
+        this.props.todoList.items.sort(this.compareByDueDate);
+        this.props.goHome();
     }
 
-    sortByItemDueDate() {
-
+    compareByDueDate =(item1,item2)=>{
+        if (this.props.todoList.currentSortCriteria=="sortByItemDueDateIncrease"){
+            if (item1.due_date < item2.due_date)
+                return -1;
+            else if (item1.due_date > item2.due_date)
+                return 1;
+            else
+                return 0;
+        }
+        else {
+            if (item1.due_date < item2.due_date)
+            return 1;
+        else if (item1.due_date > item2.due_date)
+            return -1;
+        else
+            return 0;
+        }
     }
 
-    sortByItemCompleted() {
+    sortByItemCompleted=()=> {
+        if (this.props.todoList.currentSortCriteria=="sortByItemCompletedIncrease")
+            {this.props.todoList.currentSortCriteria="sortByItemCompletedDecrease";}
+        else 
+            {this.props.todoList.currentSortCriteria="sortByItemCompletedIncrease";}
 
+        this.props.todoList.items.sort(this.compareByCompleted);
+        this.props.goHome();
+    }
+    compareByCompleted =(item1,item2) => {
+        if (this.props.todoList.currentSortCriteria=="sortByItemCompletedIncrease"){
+            if (item1.completed < item2.completed)
+                return -1;
+            else if (item1.completed > item2.completed)
+                return 1;
+            else
+                return 0;
+        }
+        else {
+            if (item1.completed < item2.completed)
+            return 1;
+        else if (item1.completed > item2.completed)
+            return -1;
+        else
+            return 0;
+        }
     }
 
 
