29c8d99cec8b17a294822d628838bd85e20e9c80
Zaitian Mou <thomasmou@outlook.com>
2019-10-13 21:14:47 -0400

    button properly disabled

--- src/components/list_screen/ListItemCard.js
+++ src/components/list_screen/ListItemCard.js
@@ -12,20 +12,20 @@ export class ListItemCard extends Component {
             return "Pending";
         }
     }
-   
+
     render() {
         // let styleColor="font-color-red";
         // if (!this.props.listItem.completed){
         //     styleColor="color: rgb(214, 10, 10);";
         // }
-        if(this.props.listItem.completed)
-        var classForWhetherCompleted="ItemCompleted";
-    
-        else 
-        var classForWhetherCompleted="ItemNotCompleted";
-    
+        if (this.props.listItem.completed)
+            var classForWhetherCompleted = "ItemCompleted";
+
+        else
+            var classForWhetherCompleted = "ItemNotCompleted";
+
         return (
-            <div className='list_item_card' onClick={()=>this.props.loadItemScreen(this.props.listItem)}>
+            <div className='list_item_card' onClick={() => this.props.loadItemScreen(this.props.listItem)}>
                 <div className='list_item_card_description'>
                     {this.props.listItem.description}
                 </div>
@@ -35,57 +35,63 @@ export class ListItemCard extends Component {
                 <div className='list_item_card_due_date'>
                     {this.props.listItem.due_date}
                 </div>
-                <div className='list_item_card_completed'  className={classForWhetherCompleted}>
+                <div className='list_item_card_completed' className={classForWhetherCompleted}>
                     {this.whetherCompleted(this.props.listItem.completed)}
                 </div>
-                {/* <script>
-            var x="ListItemMoveUp"+this.props.todoList.items.indexOf(this.props.listItem);
-    </script> */}
-                <div>
-                    <img src={MoveUp}  className="ListItemMoveUp" onClick={(e) => this.listItemMoveUp(e)}></img>
-                    {/* {this.disableButton()}
-                    {document.getElementById} */}
-                </div>
-                <div>
-                    <img src={MoveDown}  className="ListItemMoveDown" onClick={this.listItemMoveDown.bind(this)}></img>
-                </div>
-                <div>
-                    <img src={Delete} className="ListItemDelete" onClick={this.listItemDelete.bind(this)}></img>
+                <div className='list_item_card_toolbar'>
+                    <span className={'list_item_card_button' + ' ' + (this.props.todoList.items.indexOf(this.props.listItem) == 0 ? 'disabled' : '')}
+                        onClick={this.listItemMoveUp.bind(this)}
+                    > &#x21e7;</span>
+
+                    <span className={'list_item_card_button' + ' ' + (this.props.todoList.items.indexOf(this.props.listItem) == this.props.todoList.items.length - 1 ? 'disabled' : '')}
+                        onClick={this.listItemMoveDown.bind(this)}>
+                        &#x21e9;</span>
+
+                    <span className={'list_item_card_button'}
+                        onClick={this.listItemDelete.bind(this)}
+                    >&#10005;</span>
                 </div>
             </div>
         )
     }
-    disableButton =()=>{
+    nothingHappens = (event) => {
+        alert("nothg")
+    }
+    disableButton = () => {
         console.log(this.props.listItem.key);
-        
-        if (this.props.todoList.items.indexOf(this.props.listItem)==0){
+
+        if (this.props.todoList.items.indexOf(this.props.listItem) == 0) {
             //alert(this.props.listItem.key);
             console.log(document.getElementById("ListItemMoveUp"));
             //document.getElementById("ListItemMoveUp").style.pointerEvents="none";
         }
     }
-    listItemMoveUp =(event) =>{
-        event.stopPropagation();
+    listItemMoveUp = (event) => {
         let index = this.props.todoList.items.indexOf(this.props.listItem);
-        let temp=this.props.todoList.items[index-1];
-        this.props.todoList.items[index-1]=this.props.todoList.items[index];
-        this.props.todoList.items[index]=temp;
-        this.props.loadList(this.props.todoList)
+        if (index >= 1) {
+            let temp = this.props.todoList.items[index - 1];
+            this.props.todoList.items[index - 1] = this.props.todoList.items[index];
+            this.props.todoList.items[index] = 2
+            this.props.loadList(this.props.todoList)
+        }
+        event.stopPropagation();
     }
-    listItemMoveDown =(event) => {
+    listItemMoveDown = (event) => {
         event.stopPropagation();
         let index = this.props.todoList.items.indexOf(this.props.listItem);
-        let temp=this.props.todoList.items[index+1];
-        this.props.todoList.items[index+1]=this.props.todoList.items[index];
-        this.props.todoList.items[index]=temp;
-        this.props.loadList(this.props.todoList);
+        if (index < this.props.todoList.items.length - 1) {
+            let temp = this.props.todoList.items[index + 1];
+            this.props.todoList.items[index + 1] = this.props.todoList.items[index];
+            this.props.todoList.items[index] = temp;
+            this.props.loadList(this.props.todoList);
+        }
     }
-    listItemDelete =(event) =>{
+    listItemDelete = (event) => {
         event.stopPropagation();
         let index = this.props.todoList.items.indexOf(this.props.listItem);
-        this.props.todoList.items.splice(index,1);
+        this.props.todoList.items.splice(index, 1);
         this.props.loadList(this.props.todoList);
-      
+
     }
 }
 
